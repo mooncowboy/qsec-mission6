cf5a3091255b91ca56827a2020e9da78530b7793
Harald Fianbakken <harald.fianbakken@microsoft.com>
2018-11-13 15:36:53 +0100

    Added Delayed Queue

---
+++ qsec.mission6/DelayedQueue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace qsec.mission6
+{
+    class DelayedQueue<T> 
+    {
+        ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
+        public void AddAsync(T item, TimeSpan inTime)
+        {
+            var startTimeSpan = TimeSpan.Zero;
+            
+
+            var timer = new System.Threading.Timer((e) =>
+            {
+                queue.Enqueue(item);
+            }, null, startTimeSpan, inTime);
+
+        }
+    }
+}
